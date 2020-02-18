f4bfb7abbb44b93128a1eb6eb54562a448bfb4c6
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2020-02-18 21:08:42 +0100

    Details

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-let p1 = document.getElementById('page1')
-let p2 = document.getElementById('page2')
-
+const p1 = document.getElementById('page-home')
+const p2 = document.getElementById('page-right')
 let startingX
 
-/* Page 1 */
+/* Page home */
 
-p1handleTouchStart = event => {
+const p1handleTouchStart = event => {
   startingX = event.touches[0].clientX
   p1.style.transition = ''
   p2.style.transition = ''
-  //p2.style.display= 'none';
-}
+};
 
-p1handleTouchMove = event => {
-  let touch = event.touches[0]
-  let change = startingX - touch.clientX
-  
+const p1handleTouchMove = event => {
+  const touch = event.touches[0]
+  const change = startingX - touch.clientX
+
   if (change < 0) {
     return
   }
-  
-  //p2.style.display = 'inline-block'
+
   p2.style.left = (screen.width - change) + 'px'
   event.preventDefault()
-}
+};
 
-p1handleTouchEnd = event => {
-  let change = startingX - event.changedTouches[0].clientX
-  let quarter = screen.width / 4
+const p1handleTouchEnd = event => {
+  const change = startingX - event.changedTouches[0].clientX
+  const quarter = screen.width / 4
   if (change < quarter) {
     p2.style.left = '100%'
     p2.style.transition = 'all 0.3s ease 0s'
@@ -35,33 +32,31 @@ p1handleTouchEnd = event => {
     p2.style.left = '0'
     p2.style.transition = 'left 0.5s ease 0s'
   }
-}
+};
 
-/* Page 2 */
+/* Page right */
 
-p2handleTouchStart = event => {
+const p2handleTouchStart = event => {
   startingX = event.touches[0].clientX
   p1.style.transition = ''
   p2.style.transition = ''
-  //p1.style.display = 'none'
-}
+};
 
-p2handleTouchMove = event => {
-  let touch = event.touches[0]
-  let change = touch.clientX - startingX
-  
+const p2handleTouchMove = event => {
+  const touch = event.touches[0]
+  const change = touch.clientX - startingX
+
   if (change < 0) {
     return
   }
-  
-  //p1.style.display = 'inline-block'
+
   p2.style.left = change + 'px'
   event.preventDefault()
-}
+};
 
-p2handleTouchEnd = event => {
-  let change = event.changedTouches[0].clientX - startingX
-  let quarter = screen.width / 4
+const p2handleTouchEnd = event => {
+  const change = event.changedTouches[0].clientX - startingX
+  const quarter = screen.width / 4
   if (change < quarter) {
     p2.style.left = '0'
     p2.style.transition = 'all 0.3s ease 0s'
@@ -69,4 +64,4 @@ p2handleTouchEnd = event => {
     p2.style.left = '100%'
     p2.style.transition = 'left 0.5s ease 0s'
   }
-}
+};
